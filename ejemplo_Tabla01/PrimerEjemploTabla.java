0b1bca8d2f557101d70a209893cdf84b50f76260
Eva Sánchez <122869904+EvaMsanchez@users.noreply.github.com>
2023-11-20 13:30:34 +0100

    first commit

---
+++ ejemplo_Tabla01/PrimerEjemploTabla.java
@@ -0,0 +1,18 @@
+package es.studium.ejemplo_Tabla01;
+
+public class PrimerEjemploTabla
+{
+
+	public static void main(String[] args)
+	{		
+		int tablaEnteros [] = {-2, 3, 5, 0, 33, 100};	
+		
+		//Mostrar el contenido
+		for (int i=0; i<tablaEnteros.length; i++)
+		{
+			System.out.println(tablaEnteros[i]);	
+		}
+
+	}
+
+}
