29dff088dcd8ccd2121962ecb53061832bc305c0
Eva Sánchez <122869904+EvaMsanchez@users.noreply.github.com>
2023-11-20 13:35:58 +0100

    first commit

---
+++ ejercicio_Tabla11/Ejercicio11.java
@@ -0,0 +1,21 @@
+package es.studium.ejercicio_Tabla11;
+
+import java.util.Scanner;
+
+public class Ejercicio11
+{
+	/**/
+
+	public static void main(String[] args)
+	{
+		final int TAM = 3;
+		
+		int matriz [] []= new int [TAM] [TAM];
+		
+		Scanner teclado = new Scanner (System.in);
+		
+		
+
+	}
+
+}
